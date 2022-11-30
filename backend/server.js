61ef61b010f28ee9a0c434ac92336d4b2e1213a3
Alastair <alastair.chau@gmail.com>
2022-11-30 14:51:45 +0000

    added env variable

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
+require('dotenv').config() // loads environ variables into process.env object available globally
 const express = require('express')  // requires the express package
+
 
  // express app created and stored
 const app = express()
 
 // listen for requests
-app.listen(4000, () => {
-  console.log('listening on port 4000')
+// useses env variable
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+})
+
+// route handler that reacts to GET requests
+app.get('/', (req, res) => {
+  res.json({mssg: 'Welcome to the app'})
 })
 
 
